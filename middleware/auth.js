1288febbfab60badc879ca2a4c4f29decff2aa26
JoeAshraf <youssefashraf397@yahoo.com>
2021-08-01 04:34:52 +0200

    authntication is done

---
+++ middleware/auth.js
@@ -0,0 +1,10 @@
+
+
+export default function({store, redirect}) {
+    const isAuth = store.getters['auth/isAuthenticated']
+  
+    if (!isAuth) {
+      // navigate later to notAuthenticated page
+      return redirect('/notAuthnticated')
+    }
+  }
