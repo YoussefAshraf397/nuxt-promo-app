e64a38db7beefd6592a4307451d5d18df69386ad
YoussefAshraf397 <youssefashraf397@yahoo.com>
2022-02-28 17:57:39 +0200

    working on list blogs for instructor

--- pages/instructor/blog/_id/edit.vue
+++ pages/instructor/blog/_id/edit.vue
@@ -5,9 +5,10 @@
       title="Write your blog"
       exitLink="/instructor/blogs">
       <!-- TODO: Check if blog status is active -->
-      <template #actionMenu>
+      <template v-if="blog.status === 'active'" #actionMenu>
         <div class="full-page-takeover-header-button">
           <Modal
+            @submitted="updateBlogStatus($event, 'published')"
             @opened="checkBlogValidity"
             openTitle="Publish"
             openBtnClass="button is-success is-medium is-inverted is-outlined"
@@ -32,9 +33,10 @@
           </Modal>
         </div>
       </template>
-      <!-- <template v-else #actionMenu>
+      <template v-else #actionMenu>
         <div class="full-page-takeover-header-button">
           <Modal
+            @submitted="updateBlogStatus($event, 'active')"
             openTitle="Unpublish"
             openBtnClass="button is-success is-medium is-inverted is-outlined"
             title="Unpublish Blog">
@@ -43,7 +45,7 @@
             </div>
           </Modal>
         </div>
-      </template> -->
+      </template>
     </Header>
     <div class="blog-editor-container">
       <div class="container">
@@ -79,7 +81,10 @@ export default {
     ...mapState({
       blog: ({instructor}) => instructor.blog.item,
       isSaving: ({instructor}) => instructor.blog.isSaving
-    })
+    }),
+    editor() {
+      return this.$refs.editor
+    }
   },
   async fetch({params, store}) {
     await store.dispatch('instructor/blog/fetchBlogById', params.id)
@@ -98,8 +103,19 @@ export default {
         .catch(error => this.$toasted.error('Blog cannot be saved!', {duration: 2000}))
       }
     },
+    updateBlogStatus({closeModal}, status) {
+      const blogContent = this.editor.getContent()
+      blogContent.status = status
+      const message = status === 'published' ? 'Blog has been published!' : 'Blog has been un-published!'
+      this.$store.dispatch('instructor/blog/updateBlog', {data: blogContent, id: this.blog._id})
+        .then(_ => {
+          this.$toasted.success(message, {duration: 3000})
+          closeModal()
+        })
+        .catch(error => this.$toasted.error('Blog cannot be published!', {duration: 3000}))
+    },
     checkBlogValidity() {
-      const title = this.$refs.editor.getNodeValueByName('title')
+      const title = this.editor.getNodeValueByName('title')
       this.publishError = ''
       this.slug = ''
       if (title && title.length > 24) {
--- store/instructor/blog.js
+++ store/instructor/blog.js
@@ -1,5 +1,20 @@
 
+function separateBlogs(blogs) {
+  const published = []
+  const drafts = []
+
+  blogs.forEach(blog => {
+    blog.status === 'active' ? drafts.push(blog) : published.push(blog)
+  })
+
+  return {published, drafts}
+}
+
 export const state = () => ({
+  items: {
+    drafts: [],
+    published: []
+  },
   item: {},
   isSaving: false
 })
@@ -13,6 +28,17 @@ export const actions = {
   fetchBlogById({commit}, blogId) {
     return this.$axios.$get(`/api/v1/blogs/${blogId}`)
       .then(blog => commit('setBlog', blog))
+  },
+  fetchUserBlogs({commit, state}) {
+    return this.$axios.$get('/api/v1/blogs/me')
+      .then(blogs => {
+        debugger
+        const { published, drafts } = separateBlogs(blogs)
+        commit('setBlogs', {resource: 'drafts', items: drafts})
+        commit('setBlogs', {resource: 'published', items: published})
+
+        return { published, drafts }
+      })
   },
   updateBlog({commit, state}, {data, id}) {
     commit('setIsSaving', true)
@@ -32,6 +58,9 @@ export const actions = {
 export const mutations = {
   setBlog(state, blog) {
     state.item = blog
+  },
+  setBlogs(state, {resource, items}) {
+    state.items[resource] = items
   },
   setIsSaving(state, isSaving) {
     state.isSaving = isSaving
